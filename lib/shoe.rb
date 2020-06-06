e67bfd6d8ddd97919d1f5ca84bc887b551a890a4
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-06 22:16:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,26 @@
 # Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+  def brand
+    @brand
+  end
+  def color=(color)
+    @color = color
+  end
+  def color
+    @color
+  end
+  def size=(num)
+    @byte_size = num
+  end
+  def size
+    @byte_size
+  end
+  def material=(material)
+    @material = material
+  end
+  def material
+    @material
+  end
